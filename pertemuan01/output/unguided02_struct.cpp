a8bca5f0227cc1901ad49ec5cc7b4cea4678cc3e
valishaatthalia <valishaatthalia22@gmail.com>
2024-03-20 22:13:40 +0700

    modul1

---
+++ pertemuan01/output/unguided02_struct.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+struct mahasiswa_160 {
+    string nama_160;
+    string jurusan_160;
+};
+ 
+int main(){
+ 
+    mahasiswa mhs_160;
+ 
+    mhs.nama_160 = "Valisha Atthalia Naura Irfan";
+    mhs.jurusan_160 = "Teknik Informatika";
+    
+    cout << "DATA MAHASISWA" << endl;
+    cout << "Nama    : " << mhs.nama_160 << endl;
+    cout << "Jurusan : "<< mhs.jurusan_160 << endl;
+      
+    return 0;
+}
