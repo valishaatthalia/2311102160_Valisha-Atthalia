ca3b9d7194c7555a8abc6d2c7471f18d2d32c83e
valishaatthalia <valishaatthalia22@gmail.com>
2024-03-20 15:27:59 +0700

    modul3

--- pertemuan01/output/unguided02.cpp
+++ pertemuan01/output/unguided02.cpp
@@ -21,3 +21,33 @@ int main(){
       
     return 0;
 }
+
+
+#include <iostream>
+using namespace std;
+ 
+class mahasiswa {
+public:
+    int nim = 1;
+    string nama = "default name"; 
+    float nilai=0; 
+ 
+    void setterNilai (float Nilai) {
+        nilai=aNilai;
+    }
+    void printData(){
+        cout << "NIM   \t= " << nim << endl;
+        cout << "Nama  \t= " << nama << endl;
+        cout << "Nilai \t= " << nilai << endl;
+    }
+};
+ 
+int main() {
+    mahasiswa anton,budi;
+    cout<<"Anton" << endl;
+    anton.printData();
+    cout<<"==Budi==="<<endl;
+    budi.printData();
+ 
+    return 0;
+}
